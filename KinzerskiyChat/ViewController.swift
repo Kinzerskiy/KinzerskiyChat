7958b35a8cf6b23950af0993582fdd7eb045d2eb
Kinzerskiy <kinzzersky@gmail.com>
2023-06-24 14:26:23 +0300

    Initial Commit

---
+++ KinzerskiyChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KinzerskiyChat
+//
+//  Created by Mac Pro on 24.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
